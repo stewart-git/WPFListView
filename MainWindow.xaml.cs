53ef2a61483d80adf135aeab26a59304afb78bfa
stewart-git <stewart.godwin@smtafe.wa.edu.au>
2023-11-01 11:13:40 +0800

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyListView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        Queue<StaffDetails> QueueStaff = new Queue<StaffDetails>();
+        private void AddStaff_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(!string.IsNullOrEmpty(FullName.Text) && 
+                !string.IsNullOrEmpty(Position.Text) &&
+                 !string.IsNullOrEmpty(Salary.Text))
+            {
+                StaffDetails staffDetails = new StaffDetails();
+                staffDetails.SetFullName(FullName.Text);
+                staffDetails.SetPosition(Position.Text);
+                staffDetails.SetSalay(int.Parse(Salary.Text));
+                QueueStaff.Enqueue(staffDetails);
+                DisplayAllStaff();
+                ClearTextBoxes();
+            }
+        }
+        private void DisplayAllStaff()
+        {
+            ListViewDisplayStaff.Items.Clear();
+            foreach(StaffDetails staff in QueueStaff)
+            {
+                ListViewDisplayStaff.Items.Add(new
+                {
+                    StaffName = staff.GetName(),
+                    StaffPosition = staff.GetPosition(),
+                    StaffSalary = staff.GetSalary()
+                });
+            }
+        }
+        private void ClearTextBoxes()
+        {
+            FullName.Clear();
+            Position.Clear();
+            Salary.Clear();
+        }
+    }
+}
---
+++ StaffDetails.cs
@@ -0,0 +1,34 @@
+﻿namespace MyListView
+{
+    internal class StaffDetails
+    {
+        private string _fullName;
+        private string _position;
+        private int _salary;
+
+        public string GetName() 
+        {
+            return _fullName; 
+        }
+        public void SetFullName(string fullName)
+        {
+            _fullName = fullName;
+        }
+        public string GetPosition()
+        {
+            return _position;
+        }
+        public void SetPosition(string position)
+        {
+            _position = position;
+        }
+        public int GetSalary()
+        {
+            return _salary;
+        }
+        public void SetSalay(int salary)
+        {
+            _salary = salary;
+        }
+    }
+}
