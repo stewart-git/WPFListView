ac8a45ece34e2add96c7e637dc81afa4060cbc01
stewart-git <stewart.godwin@smtafe.wa.edu.au>
2023-11-03 10:21:35 +0800

    Added UI and image

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MyListView
 {
@@ -55,9 +46,24 @@ namespace MyListView
         }
         private void ClearTextBoxes()
         {
-            FullName.Clear();
-            Position.Clear();
-            Salary.Clear();
+            FullName.Clear(); FullName.Foreground = Brushes.CadetBlue; FullName.Text = "Enter Full Name";
+            Position.Clear(); Position.Foreground = Brushes.CadetBlue; Position.Text = "Enter Staff Position";     
+            Salary.Clear(); Salary.Foreground = Brushes.CadetBlue; Salary.Text = "Enter Current Salary";
+        }
+
+        private void FullName_GotFocus(object sender, RoutedEventArgs e)
+        {
+            FullName.Clear(); FullName.Foreground = Brushes.Black;
+        }
+
+        private void Position_GotFocus(object sender, RoutedEventArgs e)
+        {
+            Position.Clear(); Position.Foreground = Brushes.Black;
+        }
+
+        private void Salary_GotFocus(object sender, RoutedEventArgs e)
+        {
+            Salary.Clear(); Salary.Foreground = Brushes.Black;
         }
     }
 }
